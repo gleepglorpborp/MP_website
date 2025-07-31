852b719f97fbf7af270cfaf14bd474e7dcc62007
potassium237 <yuhaann@gmail.com>
2025-07-31 16:39:24 +0800

    Update mp.sql

--- db/mp.sql
+++ db/mp.sql
@@ -1,15 +1,62 @@
--- Drop and recreate the `mp` database
-DROP DATABASE IF EXISTS mp;
-CREATE DATABASE mp;
-USE mp;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 31, 2025 at 10:39 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
--- Create `image` table
-CREATE TABLE image (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  IMAGE VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255) NOT NULL,
-  CREATED_TIME DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (ID)
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+
+CREATE TABLE `image` (
+  `ID` int(11) NOT NULL,
+  `IMAGE` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `CREATED_TIME` datetime NOT NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE image ADD COLUMN is_deleted TINYINT(1) NOT NULL DEFAULT 0;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
