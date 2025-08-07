ff90ed365088339092cc7eb8fb413fdf8d57fe26
firebug384 <dylonlaudlws@gmail.com>
2025-08-07 15:35:34 +0800

    Update detect.php

--- php/detect.php
+++ php/detect.php
@@ -135,13 +135,13 @@ $response = $_SESSION['detection_result'] ?? null;
         }
     ?>
     <div class="cta">
-        <a href="/mp_website/php/upload.php">Upload another image</a>
+        <a href="upload.php">Upload another image</a>
     </div>
     <div class="cta">
-        <a href="/mp_website/php/history.php">View history</a>
+        <a href="history.php">View history</a>
     </div>
     <div class="cta">
-        <a href="/mp_website/php/index.php">Home Page</a>
+        <a href="index.php">Home Page</a>
     </div>
 </div>
 
